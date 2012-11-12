d65b2d0bd18bb350b4aa9bb2d9991c0882a06a95
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-11-12 01:42:34 -0300

    Add failing scenarios

---
+++ features/products_list.feature
@@ -0,0 +1,21 @@
+Feature: List of Products
+  As an user
+  I want to maintain a list of products
+  So I can pick products to write muy buy list faster
+
+  Scenario: Create a product
+    Given there are no products in the list of products
+    When I add a product to the list of products
+    Then the list of products should contain the product
+    And I should see "The new product was successfully created."
+
+  Scenario: Delete a product
+    Given there are carrots and spinach in the list of products
+    When I remove the carrots from the list of products
+    Then the list of products should not contain carrots
+    And the list of products should contain spinach
+
+  Scenario: List products
+    Given there are carrots, spinach and melon in the list of products
+    When I go to the list of products page
+    Then I should see carrots, spinach and melon
---
+++ features/step_definitions/products_list_steps.rb
@@ -0,0 +1,43 @@
+Given /^there are no products in the list of products$/ do
+end
+
+Given /^there are carrots and spinach in the list of products$/ do
+  @carrots = FactoryGirl.create(:product, name: "carrots")
+  @spinach = FactoryGirl.create(:product, name: "spinach")
+end
+
+Given /^there are carrots, spinach and melon in the list of products$/ do
+  @carrots = FactoryGirl.create(:product, name: "carrots")
+  @spinach = FactoryGirl.create(:product, name: "spinach")
+  @melon = FactoryGirl.create(:product, name: "melon")
+end
+
+When /^I add a product to the list of products$/ do
+  @spinach = FactoryGirl.create(:product)
+end
+
+When /^I remove the carrots from the list of products$/ do
+  Product.find_by_name("carrots")
+end
+
+Then /^the list of products should contain the product$/ do
+  Product.find_by_name("salad").should_not be_nil
+end
+
+Then /^I should see "(.*?)"$/ do |content|
+  page.should have_content content
+end
+
+Then /^the list of products should not contain carrots$/ do
+  Product.find_by_name("carrots").should be_nil
+end
+
+Then /^the list of products should contain spinach$/ do
+  Product.find_by_name("spinach").should_not be_nil
+end
+
+Then /^I should see carrots, spinach and melon$/ do
+  page.should have_content "carrots"
+  page.should have_content "spinach"
+  page.should have_content "melon"
+end
