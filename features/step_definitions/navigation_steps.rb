ba2e9a2f03f21eb6adc668b1e4292f7363fe210e
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-11-16 06:15:36 -0300

    Add failing scenarios

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,7 +1,35 @@
+Given /^I am on the shopping list page$/ do
+  steps %{
+    When I go to the shopping list page
+  }
+end
+
+Given /^I am on the list of products page$/ do
+  steps %{
+    When I go to the list of products page
+  }
+end
+
+Given /^I am on the new product page$/ do
+  visit new_product_path
+end
+
 When /^I go to the list of products page$/ do
   visit products_path
 end
 
 When /^I go to the shopping list page$/ do
   visit shopping_list_path
+end
+
+Then /^I should be able to go to the shopping list page$/ do
+  page.should have_selector('a[title="Go to the shopping list"]')
+end
+
+Then /^I should be able to go to the new product page$/ do
+  page.should have_selector('a[title="Create a new product"]')
+end
+
+Then /^I should be able to go to the list of products page$/ do
+  page.should have_selector('a[title="Go to the list of products"]')
 end
