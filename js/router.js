f7e07a853351ad5f2fc887a1e0f31499d9257871
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-11-11 23:07:27 -0300

    Add ShoppingListItem model data to main template

--- js/router.js
+++ js/router.js
@@ -1,3 +1,9 @@
 ShoppingList.Router.map(function () {
   this.resource('shoppingList', { path: '/' });
 });
+
+ShoppingList.ShoppingListRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('shoppingListItem');
+  }
+});
